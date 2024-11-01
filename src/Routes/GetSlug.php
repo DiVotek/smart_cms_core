efd35689a569a6bedf0d606cc817c71c17c42683
maxboyko <maxboiko@divotek.com>
2024-11-01 13:04:46 +0200

    add user binding

--- src/Routes/GetSlug.php
+++ src/Routes/GetSlug.php
@@ -5,6 +5,8 @@ namespace SmartCms\Core\Routes;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Context;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Session;
 use SmartCms\Core\Components\Pages\StaticPage;
 use SmartCms\Core\Models\Page;
 use Symfony\Component\HttpKernel\Attribute\Cache;
@@ -16,6 +18,7 @@ class GetSlug
     #[Cache(public: true, maxage: 31536000, mustRevalidate: true)]
     public function __invoke(Request $request): string
     {
+        $this->bindUser();
         $segments = $request->segments();
         if (isset($request->lang)) {
             $this->limit++;
@@ -58,5 +61,15 @@ class GetSlug
     {
         app()->setLocale($lang);
         Context::add('current_lang', $lang);
+    }
+
+    private function bindUser()
+    {
+        $uuid = Session::getId();
+        $uuid = substr($uuid, 0, 36);
+
+        if (!Cookie::get('uuid')) {
+            Cookie::queue('uuid', $uuid, 60 * 24 * 365);
+        }
     }
 }
