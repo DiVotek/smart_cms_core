43299a0c24c21362bc4a5bc5a8a367d4f8f38e10
maxboyko <maxboiko@divotek.com>
2024-11-27 21:24:00 +0200

    fix for dataLayer

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,24 +18,21 @@ Alpine.start()
 
 document.addEventListener("htmx:afterRequest", function (event) {
    const xhr = event.detail.xhr;
-   console.log(xhr.status);
-   if (xhr.status >= 300 && xhr.status < 400) {
-      const location = xhr.getResponseHeader("Location");
-      if (location) {
-         window.location.href = location;
-      }
-   }
    if (xhr.status == 200) {
       try {
          const response = JSON.parse(xhr.responseText);
-         console.log(response);
          if (response.dataLayer) {
-            console.log(response.dataLayer);
             const dataLayer = window.dataLayer || [];
             dataLayer.push(response.dataLayer);
          }
       } catch (e) {
          console.error(e);
+      }
+   }
+   if (xhr.status >= 300 && xhr.status < 400) {
+      const location = xhr.getResponseHeader("Location");
+      if (location) {
+         window.location.href = location;
       }
    }
 });
