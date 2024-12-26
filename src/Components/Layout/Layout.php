b2eec6b798d557de3829db535432b4f03b35228f
MaxBoiko21 <MaxBoiko21@users.noreply.github.com>
2024-12-26 18:42:48 +0000

    Fix styling

--- src/Components/Layout/Layout.php
+++ src/Components/Layout/Layout.php
@@ -42,24 +42,24 @@ class Layout extends Component
         }
         $this->theme = $theme;
         $this->style = Cache::remember('template_styles', 60 * 60 * 24, function () {
-            if (File::exists(scms_template_path(template()) . 'css/app.css')) {
-                return 'scms/templates/' . template() . '/css/app.css';
+            if (File::exists(scms_template_path(template()).'css/app.css')) {
+                return 'scms/templates/'.template().'/css/app.css';
             } else {
                 return '';
             }
         });
         $this->script = Cache::remember('template_scripts', 60 * 60 * 24, function () {
-            if (File::exists(scms_template_path(template()) . 'js/app.js')) {
-                return 'scms/templates/' . template() . '/js/app.js';
+            if (File::exists(scms_template_path(template()).'js/app.js')) {
+                return 'scms/templates/'.template().'/js/app.js';
             } else {
                 return '';
             }
         });
         $fav = _settings('favicon', '/favicon.ico');
-        if(str_starts_with($fav,'/')){
+        if (str_starts_with($fav, '/')) {
             $fav = substr($fav, 1);
         }
-        $this->favicon = asset('/storage/' . $fav);
+        $this->favicon = asset('/storage/'.$fav);
         $this->og_type = _settings('og_type', 'website');
     }
 
