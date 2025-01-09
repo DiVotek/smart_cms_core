d5123b790dee6cea72b728c57f59d9dac3a6fba4
MaxBoiko21 <MaxBoiko21@users.noreply.github.com>
2025-01-09 17:46:59 +0000

    Fix styling

--- src/Actions/Template/GetLinks.php
+++ src/Actions/Template/GetLinks.php
@@ -17,11 +17,11 @@ class GetLinks
         }
         $lang = current_lang_id();
 
-        return Cache::get('menu_links_' . $lang . '_' . $id, function () use ($id) {
+        return Cache::get('menu_links_'.$lang.'_'.$id, function () use ($id) {
             $menu = Menu::query()->find($id);
             if ($menu) {
                 $links = $this->parseLinks($menu->value);
-                Cache::put('menu_links_' . $id, $links, 60 * 60 * 24);
+                Cache::put('menu_links_'.$id, $links, 60 * 60 * 24);
 
                 return $links;
             }
