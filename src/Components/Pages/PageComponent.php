e45450d7f69dbe1bee097413d9f9f9f2c04cb1ba
maxboyko <maxboiko@divotek.com>
2025-01-15 13:17:13 +0200

    fix for lang error

--- src/Components/Pages/PageComponent.php
+++ src/Components/Pages/PageComponent.php
@@ -40,7 +40,7 @@ class PageComponent extends Component
     {
         $titleMod = _settings('title_mod', []);
         $descriptionMod = _settings('description_mod', []);
-        $seo = $entity->seo()->where('language_id', current_lang())->first() ?? new Seo;
+        $seo = $entity->seo()->where('language_id', current_lang_id())->first() ?? new Seo;
         $this->title = ($titleMod->prefix ?? '').($seo->title ?? '').($titleMod->suffix ?? '');
         $this->meta_description = ($descriptionMod->prefix ?? '').($seo->description ?? '').($descriptionMod->suffix ?? '');
         $this->meta_keywords = $seo->meta_keywords ?? '';
