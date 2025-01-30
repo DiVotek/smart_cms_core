fd10172690ef1bda27f6cc5b3ebd22504df9b81e
maxboyko <maxboiko@divotek.com>
2025-01-30 13:53:33 +0200

    fix for migrations

---
+++ database/migrations/0021_alter_pages_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use SmartCms\Core\Models\Layout;
+use SmartCms\Core\Models\Page;
+
+return new class extends Migration
+{
+   public function up(): void
+   {
+      Schema::table(Page::getDb(), function (Blueprint $table) {
+         if (! Schema::hasColumn(Page::getDb(), 'layout_id')) {
+            $table->foreignId('layout_id')->nullable();
+         }
+      });
+   }
+
+   public function down(): void
+   {
+      Schema::table(Page::getDb(), function (Blueprint $table) {
+         if (Schema::hasColumn(Page::getDb(), 'layout_id')) {
+            $table->dropColumn('layout_id');
+         }
+      });
+   }
+};
