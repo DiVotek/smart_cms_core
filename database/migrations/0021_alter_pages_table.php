f0d6448f1a89b65e928d100b3b8242458a4d926e
MaxBoiko21 <92517114+MaxBoiko21@users.noreply.github.com>
2025-01-30 11:53:55 +0000

    Fix styling

--- database/migrations/0021_alter_pages_table.php
+++ database/migrations/0021_alter_pages_table.php
@@ -3,26 +3,25 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use SmartCms\Core\Models\Layout;
 use SmartCms\Core\Models\Page;
 
 return new class extends Migration
 {
-   public function up(): void
-   {
-      Schema::table(Page::getDb(), function (Blueprint $table) {
-         if (! Schema::hasColumn(Page::getDb(), 'layout_id')) {
-            $table->foreignId('layout_id')->nullable();
-         }
-      });
-   }
+    public function up(): void
+    {
+        Schema::table(Page::getDb(), function (Blueprint $table) {
+            if (! Schema::hasColumn(Page::getDb(), 'layout_id')) {
+                $table->foreignId('layout_id')->nullable();
+            }
+        });
+    }
 
-   public function down(): void
-   {
-      Schema::table(Page::getDb(), function (Blueprint $table) {
-         if (Schema::hasColumn(Page::getDb(), 'layout_id')) {
-            $table->dropColumn('layout_id');
-         }
-      });
-   }
+    public function down(): void
+    {
+        Schema::table(Page::getDb(), function (Blueprint $table) {
+            if (Schema::hasColumn(Page::getDb(), 'layout_id')) {
+                $table->dropColumn('layout_id');
+            }
+        });
+    }
 };
