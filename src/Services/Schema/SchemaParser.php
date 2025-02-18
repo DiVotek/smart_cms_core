055e8f966ecb450a0717ea007aa34ec57df90b34
MaxBoiko21 <92517114+MaxBoiko21@users.noreply.github.com>
2025-02-18 15:11:12 +0000

    Fix styling

--- src/Services/Schema/SchemaParser.php
+++ src/Services/Schema/SchemaParser.php
@@ -91,7 +91,7 @@ class SchemaParser
                     break;
                 }
                 if (! str_contains($fieldValue, 'http')) {
-                    $fieldValue = 'storage/' . $fieldValue;
+                    $fieldValue = 'storage/'.$fieldValue;
                 }
                 $value = asset($fieldValue);
                 $value = preg_replace('#(?<!:)//+#', '/', $value);
