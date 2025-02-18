17ad48a742fe6c2dd3c594184087ab592e404885
MaxBoiko21 <92517114+MaxBoiko21@users.noreply.github.com>
2025-02-18 09:33:16 +0000

    Fix styling

--- src/Middlewares/NoIndex.php
+++ src/Middlewares/NoIndex.php
@@ -7,13 +7,14 @@ use Illuminate\Http\Request;
 
 class NoIndex
 {
-   public function handle(Request $request, Closure $next)
-   {
-      $response = $next($request);
-      $response->headers->set('X-Robots-Tag', 'noindex, nofollow');
-      $response->headers->set('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-      $response->headers->set('Pragma', 'no-cache');
-      $response->headers->set('Expires', '0');
-      return $response;
-   }
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+        $response->headers->set('X-Robots-Tag', 'noindex, nofollow');
+        $response->headers->set('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Expires', '0');
+
+        return $response;
+    }
 }
