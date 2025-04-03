eff128f781e3becbf8c1229cc7c874a76e99fa73
MaxBoiko21 <92517114+MaxBoiko21@users.noreply.github.com>
2025-04-03 15:34:33 +0000

    Fix styling

--- src/Admin/Pages/Settings/Settings.php
+++ src/Admin/Pages/Settings/Settings.php
@@ -222,7 +222,7 @@ class Settings extends BaseSettings
                     $theme = array_merge(config('theme', []), $theme);
                     $schema = [];
                     foreach ($theme as $key => $value) {
-                        $schema[] = ColorPicker::make('theme.' . $key)
+                        $schema[] = ColorPicker::make('theme.'.$key)
                             ->label(ucfirst($key))
                             ->default($value);
                     }
