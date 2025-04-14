59e643a2783f659822ac89289ce96f4e7a94b5c8
MaxBoiko21 <92517114+MaxBoiko21@users.noreply.github.com>
2025-04-14 13:20:18 +0000

    Fix styling

--- src/Admin/Resources/StaticPageResource.php
+++ src/Admin/Resources/StaticPageResource.php
@@ -140,7 +140,7 @@ class StaticPageResource extends BaseResource
         }
         $imagePath = '';
         if ($form->getRecord()->slug) {
-            $imagePath = 'pages/' . $form->getRecord()->slug;
+            $imagePath = 'pages/'.$form->getRecord()->slug;
         }
 
         return [
@@ -166,13 +166,13 @@ class StaticPageResource extends BaseResource
                                 Forms\Components\Placeholder::make('created_at')
                                     ->label(_fields('created_at'))
                                     ->inlineLabel()
-                                    ->content(fn($record): ?string => $record->created_at?->diffForHumans()),
+                                    ->content(fn ($record): ?string => $record->created_at?->diffForHumans()),
 
                                 Forms\Components\Placeholder::make('updated_at')
                                     ->label(_fields('updated_at'))
                                     ->translateLabel()
                                     ->inlineLabel()
-                                    ->content(fn($record): ?string => $record->updated_at?->diffForHumans()),
+                                    ->content(fn ($record): ?string => $record->updated_at?->diffForHumans()),
                                 Schema::getStatus()->hidden(function ($record) {
                                     return $record->slug == '' || MenuSection::query()->where('parent_id', $record->id)->exists();
                                 }),
