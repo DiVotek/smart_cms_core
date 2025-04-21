955ac4c1030d7408f42d06fd2035b18b7b370520
maxboyko <maxboiko@divotek.com>
2025-04-21 13:34:21 +0300

    change color palette

--- config/theme.php
+++ config/theme.php
@@ -1,19 +1,34 @@
 <?php
 
 return [
-    'surface-main' => '#f28c00',
-    'surface-raised' => '#dce7ed',
-    'surface-default' => '#fff',
-    'surface-lower' => '#465761',
-    'surface-border' => '#dce7ed',
-    'text-heading' => '#000',
-    'text-normal' => '#717374',
-    'text-accent' => '#fff',
-    'text-link' => '#f28c00',
+    // background colors
+    'primary' => '#000',
+    'secondary' => '#fff',
+    'accent' => '#000',
+
+    // shades of gray
+    'base-200' => '#dce7ed',
+    'base-500' => '#465761',
+    'base-800' => '#000',
+
+    // text colors
+    'regular' => '#000',
+    'heading' => '#000',
+    'muted' => '#717374',
+
+    // status colors
+    'success' => '#3e8443',
+    'warning' => '#ff0000',
+    'danger' => '#ff0000',
+    'info' => '#000',
+
+    // base colors
+    'black' => '#000',
+    'white' => '#fff',
+
+    // modal backdrop
     'modal-backdrop' => '#00000080',
-    'focus' => '#f28c00',
-    'text-success' => '#3e8443',
-    'surface-success' => '#d6f2da',
-    'text-error' => '#ff0000',
-    'surface-error' => '#ffbabb',
+
+    // outline
+    'outline' => '#f28c00',
 ];
