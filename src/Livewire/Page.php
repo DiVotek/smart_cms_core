fbdf4da9f52b1da331a6552503e1d26d6c0f0b2c
maxboyko <maxboiko@divotek.com>
2025-06-10 17:05:55 +0300

    fix for page templates

--- src/Livewire/Page.php
+++ src/Livewire/Page.php
@@ -4,6 +4,7 @@ namespace SmartCms\Core\Livewire;
 
 use SmartCms\Core\Extenders\Pages\PageExtender;
 use SmartCms\Core\Models\Layout;
+use SmartCms\Core\Models\MenuSection;
 use SmartCms\Core\Resources\PageEntityResource;
 use SmartCms\Core\Support\Livewire\Page as LivewirePage;
 
@@ -18,11 +19,36 @@ class Page extends LivewirePage
 
     public function getView(): string
     {
-        return 'layouts.'.$this->pageLayout?->path;
+        return 'layouts.' . $this->pageLayout?->path;
     }
 
     public function getEntity(): object
     {
         return PageEntityResource::make($this->model)->get();
+    }
+
+    protected function getTemplate(): array
+    {
+        $template = $this->model->template()->select([
+            'template_section_id',
+            'value',
+        ])->get()->toArray();
+        if (empty($template)) {
+            $parent = $this->model->parent;
+            if (!$parent) {
+                return [];
+            }
+            $parent_id = $parent->parent_id ?? $parent->id;
+            $menuSection = MenuSection::query()->where('parent_id', $parent_id)->first();
+            if ($menuSection) {
+                if ($parent->parent_id) {
+                    return $menuSection->template ?? [];
+                } else {
+                    return $menuSection->categories_template ?? [];
+                }
+            }
+            return [];
+        }
+        return $template;
     }
 }
