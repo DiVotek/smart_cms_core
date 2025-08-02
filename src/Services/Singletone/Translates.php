29b3d3a250752cf63ef6ac185bfdd674ac5c3d1d
maxboyko <maxboiko@divotek.com>
2025-08-02 13:41:43 +0300

    optimize translates for livewire/update

--- src/Services/Singletone/Translates.php
+++ src/Services/Singletone/Translates.php
@@ -2,19 +2,21 @@
 
 namespace SmartCms\Core\Services\Singletone;
 
+use Illuminate\Support\Collection;
 use SmartCms\Core\Models\Translation;
 
 class Translates
 {
-    public $translates;
+    public ?Collection $translates = null;
 
-    public function __construct()
-    {
-        $this->translates = Translation::query()->where('language_id', current_lang_id())->get();
-    }
+    public function __construct() {}
 
     public function get(string $key): string
     {
+        if (! $this->translates) {
+            $this->translates = Translation::query()->where('language_id', current_lang_id())->get();
+        }
+
         $translate = $this->translates->where('key', $key)->first();
         if (! $translate) {
             return $key;
